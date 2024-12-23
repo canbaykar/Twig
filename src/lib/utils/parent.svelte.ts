02cdf33ec286c2096fc02f76ecb89684ed5c9fae
Can <togancan42@gmail.com>
2024-12-23 06:13:11 +0300

    refactor: Add Parent and Child class utilities

---
+++ src/lib/utils/parent.svelte.ts
@@ -0,0 +1,75 @@
+// The methods of Child and Parent immediately affect one another.
+// For example a.append(b) immediately updates a.parent and b.children.
+
+export class Child {
+    /** ($state) */
+    readonly parent: Parent | null = $state(null);
+
+    detach() {
+        if (this.parent) this.parent.detachChild(this);
+    }
+
+    attach(parent: Parent, index?: number) {
+        parent.attachChild(this, index);
+    }
+}
+
+export class Parent extends Child {
+    /** ($state.raw) */
+    readonly children: Child[] = $state.raw([]);
+
+    attachChild(child: Child, index?: number) {
+        if (child.parent) child.parent.detachChild(child);
+        // @ts-expect-error
+        this.children = index === undefined
+            ? [...this.children, child]
+            : this.children.toSpliced(index, 0, child);
+        // @ts-expect-error
+        child.parent = this;
+    }
+
+
+    detachChild(child: Child) {
+        const index = this.children.indexOf(child);
+        if (index === -1) return false;
+        // @ts-expect-error
+        this.children = this.children.toSpliced(index, 1);
+        // @ts-expect-error
+        child.parent = null;
+    }
+}
+
+
+// --- TESTS ---
+if (import.meta.vitest) {
+    const { it, expect } = import.meta.vitest;
+
+    it('Attaching-Detaching', () => {
+        class Named extends Child { name = ''; }
+
+        const c0 = new Named();
+        c0.name = 'c0';
+        const c1 = new Named();
+        c1.name = 'c1';
+        const p0 = new Parent();
+        const p1 = new Parent();
+
+        expect(c0.parent).toBe(null);
+        expect(p0.children).toEqual([]);
+
+        p0.attachChild(c0);
+        c1.attach(p0);
+
+        expect(c0.parent).toBe(p0);
+        expect(c1.parent).toBe(p0);
+        expect(p0.children).toEqual([c0, c1]);
+
+        c0.attach(p1);
+        c1.attach(p1, 0);
+
+        expect(c0.parent).toBe(p1);
+        expect(c1.parent).toBe(p1);
+        expect(p0.children).toEqual([]);
+        expect(p1.children).toEqual([c1, c0]);
+    });
+}
