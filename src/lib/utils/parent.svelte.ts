296e465fa4a88ee301b1f208e034c0e22a262d23
Can Baykar <togancan42@gmail.com>
2025-02-11 02:54:41 +0300

    feat: Add depth to Child

--- src/lib/utils/parent.svelte.ts
+++ src/lib/utils/parent.svelte.ts
@@ -2,8 +2,10 @@
 // For example a.append(b) immediately updates a.parent and b.children.
 
 export class Child {
-    /** ($state.raw) */
+    /** ($state.raw, readonly) */
     readonly parent: Parent | null = $state.raw(null);
+    /** ($state, readonly) */
+    readonly depth: number = $state(0);
 
     detach() {
         if (this.parent) this.parent.detachChild(this);
@@ -15,7 +17,7 @@ export class Child {
 }
 
 export class Parent extends Child {
-    /** ($state.raw) */
+    /** ($state.raw, readonly) */
     readonly children: Child[] = $state.raw([]);
 
     attachChild(child: Child, index?: number) {
@@ -26,6 +28,7 @@ export class Parent extends Child {
             : this.children.toSpliced(index, 0, child);
         // @ts-expect-error
         child.parent = this;
+        updateDepth(child, this.depth + 1, this);
     }
 
     attachChildren(children: Child[], index?: number) {
@@ -36,8 +39,13 @@ export class Parent extends Child {
         this.children = index === undefined
             ? [...this.children, ...children]
             : this.children.toSpliced(index, 0, ...children);
-        // @ts-expect-error
-        children.parent = this;
+        
+        const childDepth = this.depth + 1;
+        for (const ch of children) {
+            // @ts-expect-error
+            ch.parent = this;
+            updateDepth(ch, childDepth, this);
+        }
     }
 
     detachChild(child: Child) {
@@ -47,7 +55,19 @@ export class Parent extends Child {
         this.children = this.children.toSpliced(index, 1);
         // @ts-expect-error
         child.parent = null;
+        updateDepth(child);
     }
+}
+
+// self is requested for circularity checks.
+// depth should be self.depth + 1. Requested to avoid unnecessary recalculation.
+// May throw RangeError (for stack overflow) if the tree is too deep :(
+function updateDepth(target: Child, depth = 0, self?: Parent) {
+    // @ts-expect-error
+    target.depth = depth;
+    if (!(target instanceof Parent)) return;
+    if (target === self) throw new Error("Circularity: Parent is it's own descendant");
+    target.children.forEach(ch => updateDepth(ch, depth + 1, self));
 }
 
 
@@ -57,7 +77,6 @@ if (import.meta.vitest) {
 
     it('Attaching-Detaching', () => {
         class Named extends Child { name = ''; }
-
         const c0 = new Named();
         c0.name = 'c0';
         const c1 = new Named();
@@ -82,5 +101,28 @@ if (import.meta.vitest) {
         expect(c1.parent).toBe(p1);
         expect(p0.children).toEqual([]);
         expect(p1.children).toEqual([c1, c0]);
+    });
+
+    it('Parent Depth', () => {
+        const [A, B, C] = [0,0,0].map(n => new Parent());
+
+        expect(B.parent).toBe(null);
+        expect(A.depth).toBe(0);
+        expect(B.depth).toBe(0);
+        expect(C.depth).toBe(0);
+
+        A.attachChild(B);
+        C.attach(B);
+
+        expect(B.parent).toBe(A);
+        expect(A.depth).toBe(0);
+        expect(B.depth).toBe(1);
+        expect(C.depth).toBe(2);
+
+        B.detach();
+
+        expect(A.depth).toBe(0);
+        expect(B.depth).toBe(0);
+        expect(C.depth).toBe(1);
     });
 }
