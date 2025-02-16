0d54a757dbd683a63595c7851ccb3bd1bae81d83
Can Baykar <togancan42@gmail.com>
2025-02-16 11:53:09 +0300

    refactor(state): Add example Deriv

--- src/lib/state/deriv.svelte.ts
+++ src/lib/state/deriv.svelte.ts
@@ -1,5 +1,6 @@
 import DerivRenderData, { type SDerivRenderData } from "$lib/components/viewport/deriv/renderData.svelte";
 import { Parent } from "$lib/utils/parent.svelte";
+import viewport from "./viewport.svelte";
 
 /** Serialized Deriv */
 export declare interface SDeriv {
@@ -33,3 +34,53 @@ export default class Deriv extends Parent {
         this.render.callOnParentChange(newParent);
     }
 }
+
+// Attach example deriv
+viewport.attachChild(
+    new Deriv({
+        conc: '(A∧{Φ})→C',
+        // rule: '→I',
+        // label: '1',
+        render: {
+            x: -130,
+            y: 70,
+        },
+        children: [
+            {
+                conc: 'C',
+                // rule: '→E',
+                children: [
+                    {
+                        conc: '{Φ}',
+                        // rule: '∧E',
+                        children: [
+                            {
+                                conc: 'A∧{Φ}',
+                                // rule: '1',
+                            }
+                        ]
+                    },
+                    {
+                        conc: '{Φ}→C',
+                        // rule: '→E',
+                        children: [
+                            {
+                                conc: 'A',
+                                // rule: '∧E',
+                                children: [
+                                    {
+                                        conc: 'A∧{Φ}',
+                                        // rule: '1'
+                                    }
+                                ]
+                            },
+                            {
+                                conc: 'A→({Φ}→C)'
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    })
+);
