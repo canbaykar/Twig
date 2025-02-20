5eaee3d35c160e14537548c146cd0f25d65519a1
Can <togancan42@gmail.com>
2025-02-21 00:41:07 +0300

    fix(ui): Account for intial font with data.render.width

--- src/lib/components/viewport/deriv/deriv.svelte
+++ src/lib/components/viewport/deriv/deriv.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { browser } from "$app/environment";
 	import type Deriv from "$lib/state/deriv.svelte";
 
     interface Props {
@@ -12,6 +13,11 @@
     let element: HTMLElement;
 
     // Maintain data.render.width
+    // Initial set (annoyingly have to account for font load)
+    if (browser) 
+        document.fonts.load('12px "M PLUS 1p"').then(() => {
+            data.render.width = element.offsetWidth;
+        });
     // TODO: Test that this only works when conc changes!
     $effect(() => {
         data.conc;
