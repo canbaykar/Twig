b59cf1895faf3ec296eb22cd29fd4956ef222cb7
Can Baykar <togancan42@gmail.com>
2025-02-27 19:14:54 +0300

    style(ui): Make DraggableOptions type clearer by n->target

--- src/lib/utils/interact/draggable.svelte.ts
+++ src/lib/utils/interact/draggable.svelte.ts
@@ -11,7 +11,7 @@ export interface DraggableOptions {
 	start?(e: MouseEvent): void;
 	move?(e: MouseEvent & { dx: number; dy: number }): void;
 	cursor?: string;
-	checker?: (n: HTMLElement) => boolean;
+	checker?: (target: HTMLElement) => boolean;
 }
 
 // So that only one drag interaction activates at once
