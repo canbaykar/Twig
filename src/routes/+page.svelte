9b7a86d909de3d2ea59ebe74158507d2e4b819c8
Can Baykar <togancan42@gmail.com>
2025-03-06 09:19:17 +0300

    style: Comments to help debugging
    Commented out logging code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,12 @@
 	import './global.css';
 	import Viewport from "$lib/components/viewport/viewport.svelte";
 	import viewport from "$lib/state/viewport.svelte";
+
+	// import grammar from "$lib/state/logic/grammar";
+	// console.clear();
+	// const parsed = grammar.parse(' (A∧B)→[0] ');
+	// console.log(parsed + '');
+	// console.log(JSON.stringify(parsed, null, 2));
 </script>
 
 <Viewport {viewport} />
