11d94cc80748cc80c51dcdb8dfb81c29cca189ce
Can Baykar <togancan42@gmail.com>
2025-03-27 01:11:41 +0300

    build(ui): Update Shadcn
    Note: This projects doesn't yet use shadcn and doesn't have tailwind.config and app.css adjusted for it. Referred tailwind utilities should be substituted or added as needed while importing from shadcn, shadcn components won't work out-of-the-box. This is intentional.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
 import type { Config } from "tailwindcss";
 import { DTPlugin } from './src/DT';
+import tailwindcssAnimate from 'tailwindcss-animate';
 
 const config: Config = {
-	plugins: [DTPlugin],
+	plugins: [DTPlugin, tailwindcssAnimate],
 };
 
 export default config;
