73fb170ca9c7ad26ae96841cc5c63e3f1b7cada7
Can Baykar <togancan42@gmail.com>
2025-03-29 02:23:08 +0300

    feat(ui): Implement DND WIP (only detach & move)
    Only has detathing and moving functionality yet, not linking nodes.

--- src/lib/components/viewport/deriv/deriv.svelte
+++ src/lib/components/viewport/deriv/deriv.svelte
@@ -3,6 +3,9 @@
 	import type Deriv from "$lib/state/deriv.svelte";
     import Formula from './formula.svelte';
     import GripVertical from '~icons/lucide/grip-vertical';
+	import type { DraggableOptions } from '$lib/utils/interact/draggable.svelte';
+	import draggable from '$lib/utils/interact/draggable.svelte';
+	import viewport from '$lib/state/viewport.svelte';
 
     interface Props {
         data: Deriv;
@@ -14,15 +17,39 @@
     let label = $state('1');
     let rule = $state('∧E');
     // let rule = $state('2');
+    
+    const opt: DraggableOptions = {
+        cursor: "all-scroll",
+        start(e) {
+            if (data.parent !== viewport) {
+                const x = data.render.x;
+                const y = data.render.y;
+                data.attach(viewport);
+                data.render.xTransform = x;
+                data.render.yTransform = y;
+            }
+        },
+        move(e) {
+            data.render.xTransform += e.dx * viewport.render.screen2viewport;
+            data.render.yTransform += e.dy * viewport.render.screen2viewport;
+        },
+    };
 </script>
 
 <div class="deriv" style:translate="{data.render.x}px {data.render.y}px">
     <Formula {data}>
-        <GripVertical width="0.75em" height="100%" class="right-[100%] popup inset-y-0" />
-        <GripVertical width="0.75em" height="100%" class="left-[100%]  popup inset-y-0" />
+        {@render handle("right-[100%]")}
+        {@render handle("left-[100%]")}
     </Formula>
     <Bar {data} {rule} {label} />
 </div>
+
+<!-- TODO: Remove relative! -->
+{#snippet handle(tw: string)}
+    <div use:draggable={opt} class="{tw} popup inset-y-0 cursor-all-scroll flex items-center">
+        <GripVertical width="0.75em" height="0.75em" class="relative!" />
+    </div>
+{/snippet}
 
 <style>
     /* Preprocessor removes it if I do ".popup" 😑 */
