039fdc7910c2a48fcf348225c2dd3b16a890f39f
Can Baykar <togancan42@gmail.com>
2025-03-29 04:51:13 +0300

    feat(ui): Take dragged Derivs to front

--- src/lib/components/viewport/deriv/deriv.svelte
+++ src/lib/components/viewport/deriv/deriv.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     import Bar from './bar.svelte';
-	import type Deriv from "$lib/state/deriv.svelte";
+	import Deriv from '$lib/state/deriv.svelte';
     import Formula from './formula.svelte';
     import GripVertical from '~icons/lucide/grip-vertical';
 	import type { DraggableOptions } from '$lib/utils/interact/draggable.svelte';
 	import draggable from '$lib/utils/interact/draggable.svelte';
 	import viewport from '$lib/state/viewport.svelte';
+	import { DT } from '../../../../DT';
 
     interface Props {
         data: Deriv;
@@ -21,17 +22,24 @@
     const opt: DraggableOptions = {
         cursor: "all-scroll",
         start(e) {
-            if (data.parent !== viewport) {
+            if (data.parent === viewport)
+                // Detach & reattach to get to front
+                data.attach(viewport); 
+            else {
                 const x = data.render.x;
                 const y = data.render.y;
                 data.attach(viewport);
                 data.render.xTransform = x;
                 data.render.yTransform = y;
             }
+                
         },
         move(e) {
             data.render.xTransform += e.dx * viewport.render.screen2viewport;
             data.render.yTransform += e.dy * viewport.render.screen2viewport;
+            // Calculate center point
+            const x = data.render.x + data.render.width / 2;
+            const y = data.render.y + DT.derivLineHeightN / 2;
         },
     };
 </script>
