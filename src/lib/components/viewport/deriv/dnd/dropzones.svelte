d88d5130c824d47bcc626426a1bbe78d2ae88aec
Can Baykar <togancan42@gmail.com>
2025-04-07 03:51:59 +0300

    refactor(ui): Make positioning logic exportable for DND scripts outside this file

--- src/lib/components/viewport/deriv/dnd/dropzones.svelte
+++ src/lib/components/viewport/deriv/dnd/dropzones.svelte
@@ -1,45 +1,72 @@
-<script lang="ts">
+<script module lang="ts">
 	import Deriv from '$lib/state/deriv.svelte';
 	import { DT } from '../../../../../DT';
 
+    export type DropzoneType = keyof typeof dropzonePositioner;
+
+    const row2height = (row: number) => row * DT.derivRowOffsetN - DT.derivBarBottomN;
+    /** Functions by zone type, returning (numbers) { left: number, top: number }
+     *  (relative to the deriv that renders the zone). */
+    export const dropzonePositioner = {
+        root(deriv: Deriv): { left: number; top: number } {
+            return { 
+                left: -deriv.render.width / 2 - DT.derivDropZonePaddingN, 
+                top: row2height(0),
+            };
+        },
+        bottom(deriv: Deriv): { left: number; top: number } {
+            return { 
+                left: -Math.min(deriv.render.width / 2 + DT.derivDropZonePaddingN, deriv.render.barWidth / 2), 
+                top: row2height(1),
+            };
+        },
+        top(deriv: Deriv): { left: number; top: number } {
+            return { 
+                left: -deriv.render.barWidth / 2, 
+                top: row2height(-1),
+            };
+        },
+        child(deriv: Deriv): { left: number; top: number } {
+            const c0r = deriv.children[0].render;
+            return {
+                left: Math.min(-deriv.render.barWidth / 2, c0r.x - deriv.render.x - c0r.width / 2 - DT.derivDropZonePaddingN), 
+                top: row2height(-1),
+            };
+        }
+    };
+</script>
+
+<script lang="ts">
     interface Props {
         data: Deriv;
     }
-    
+
     let { data }: Props = $props();
-    
-    const render = data.render;
-    const padding = DT.derivDropZonePaddingN;
-    const barWidth2 = render.barWidth / 2;
-    const N = data.children.length - 1;
 </script>
 
 {#if data.root === data}
-    {@const right = render.width / 2 + padding}
     <!-- Root -->
-    {@render dropzone(-right, right, 0)}
+    {@render dropzone('root')}
     <!-- Bottom -->
-    {@render dropzone(Math.max(-barWidth2, -right), Math.min(barWidth2, right), 1)}
+    {@render dropzone('bottom')}
 {/if}
 
-{#if N === -1}
+{#if data.children.length === 0}
     <!-- Top -->
-    {@render dropzone(-barWidth2, barWidth2)}
+    {@render dropzone('top')}
 {:else}
-    {@const x = render.x}
-    {@const c0r = data.children[0].render}
-    {@const cNr = data.children[N].render}
     <!-- Leaf -->
-    {@render dropzone(Math.min(-barWidth2, c0r.x - x - c0r.width / 2 - padding), Math.max(cNr.x - x + cNr.width / 2 + padding, barWidth2))}
+    {@render dropzone('child')}
 {/if}
 
 <!-- SNIPPETS -->
-{#snippet dropzone(left: number, right: number, y = -1)}
+{#snippet dropzone(type: DropzoneType)}
+    {@const op = dropzonePositioner[type](data)}
     <div
-        class="dropzone outline-10 h-(--DERIV-ROW-OFFSET) bg-amber-500/10 [.active-dropzone]:bg-sky-500/10"
-        style:left="{left}px"
-        style:width="{right - left}px"
-        style:top="{y * DT.derivRowOffsetN - DT.derivBarBottomN}px"
+        class="dropzone outline-10 h-(--DERIV-ROW-OFFSET) bg-amber-500/10"
+        style:left="{op.left}px"
+        style:width="{-2 * op.left}px"
+        style:top="{op.top}px"
         data-address={data.address}
     ></div>
 {/snippet}
