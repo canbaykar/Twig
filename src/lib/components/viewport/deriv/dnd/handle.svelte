b724b099572eaaada692d92710310de28f6595d8
Can Baykar <togancan42@gmail.com>
2025-04-08 05:19:44 +0300

    feat(ui): Add proper dropzone detection WIP

--- src/lib/components/viewport/deriv/dnd/handle.svelte
+++ src/lib/components/viewport/deriv/dnd/handle.svelte
@@ -7,6 +7,7 @@
 	import { DT } from '../../../../../DT';
 	import { IndicatorPopup } from './indicatorPopup.svelte';
 	import { dragLog } from '../deriv.svelte';
+	import { dropzonePositioner, type DropzoneType } from './dropzones.svelte';
 
     interface Props {
         data: Deriv;
@@ -20,6 +21,75 @@
         dragged = $bindable(false),
         ...restProps
     }: Props = $props();
+
+    type DropAction = null | {
+        deriv: Deriv;
+        type: DropzoneType;
+        childIndex: number;
+    }
+
+    /** Takes in world coords, not screen! */
+    function DAFromPoint(x: number, y: number): DropAction {
+        const wrld2cl = viewport.render.wrld2cl;
+        const el = document.elementFromPoint(wrld2cl.x(x), wrld2cl.y(y));
+        // if ((el instanceof HTMLElement) && !el.classList.contains('dropzone')) console.log(el)
+        if (!(el instanceof HTMLElement) || !el.classList.contains('dropzone')) 
+            return null;
+        
+            const type = el.dataset.type as DropzoneType | undefined;
+            const adr = el.dataset.address;
+            if (!type || !adr) return null;
+            const deriv = Deriv.lookup(adr);
+            if (!deriv) return null;
+
+            switch (type) {
+                case 'child':
+                    for (let i = deriv.children.length - 1; i >= 0; i--)
+                        if (x > deriv.children[i].render.x)
+                            return { childIndex: i + 1, deriv, type: 'child' };
+                    return { childIndex: 0, deriv, type: 'child' };
+                case 'top':
+                    return { childIndex: 0, deriv, type: 'top' };
+                case 'root':
+                    return { childIndex: x > deriv.render.x ? 1 : 0, deriv, type: 'root' };
+                default: // case 'bottom'
+                    return { childIndex: 0, deriv, type: 'bottom' };
+            }
+    }
+
+    function executeDA(a: DropAction) {
+        // ...
+    }
+    
+    function indicateDA(a: DropAction, ind: IndicatorPopup) {
+        if (!a) {
+            // If a is null, make ind invisible but keep it on dragged deriv for animation
+            ind.opacity = 0;
+            ind.left = data.render.x - data.render.width / 2;
+            ind.top = data.render.y - DT.derivBarBottomN;
+            ind.width = data.render.width;
+            return;
+        }
+        const pos = dropzonePositioner[a.type](a.deriv);
+        const render = a.deriv.render;
+        ind.top = render.y + pos.top;
+        ind.opacity = 1;
+
+        const left = render.x + pos.left;
+        const right = render.x - pos.left;
+        console.log({left, ...a})
+        if (a.type !== 'child') {
+            ind.left = left;
+            ind.width = right - left;
+        } else {
+            const c = a.deriv.children;
+            // Centers of left and right siblings
+            const leftCenter  = c[a.childIndex - 1]?.render?.x ?? -Infinity;
+            const rightCenter = c[a.childIndex    ]?.render?.x ??  Infinity;
+            ind.left = Math.max(left, leftCenter);
+            ind.width = Math.min(right, rightCenter) - ind.left;
+        }
+    }
     
     const opt: DraggableOptions = {
         cursor: "all-scroll",
@@ -34,64 +104,38 @@
                 data.render.xTransform = x;
                 data.render.yTransform = y;
             }
+
             dragged = true;
             dragLog(true);
 
-            // let activeZone: null | Element = null;
-            // function updateActiveZone(val: null | Element) {
-            //     if (val && !val.classList.contains("dropzone")) val = null;
-            //     if (val === activeZone) return;
-            //     if (activeZone) activeZone.classList.remove('active-dropzone');
-            //     if (val) val.classList.add('active-dropzone');
-            //     activeZone = val;
-            // }
-
+            let dropAction: DropAction = null;
+            // Rectangle popup that indicates current dropAction
             const indicator = new IndicatorPopup();
             indicator.height = DT.derivRowOffsetN;
 
-            function updateIndicator() {
-                indicator.left = data.render.x - data.render.width / 2;
-                indicator.top = data.render.y - DT.derivBarBottomN;
-                indicator.width = data.render.width;
-                indicator.opacity = 1;
+            function updateDA() {
+                const x = data.render.x;
+                const y = data.render.y;
+                dropAction = DAFromPoint(x, y);
+                indicateDA(dropAction, indicator); 
             }
-            updateIndicator();
 
             return {
                 move(e) {
-                    updateIndicator();
-
                     const cl2wrld = viewport.render.cl2wrld;
-                    const wrld2cl = viewport.render.wrld2cl;
-                    
-                    // Move
                     data.render.xTransform += cl2wrld.scale(e.dx);
                     data.render.yTransform += cl2wrld.scale(e.dy);
-                    
-                    // Root formula center
-                    const x = data.render.x;
-                    const y = data.render.y - DT.derivLineHeightN / 2;
-                    const el = document.elementFromPoint(
-                        wrld2cl.x(x),
-                        wrld2cl.y(y),
-                    );
 
-                    // updateActiveZone(el);
+                    updateDA();
                 },
 
                 end(e) {
                     dragged = false;
                     dragLog(false);
 
-                    // const zone = activeZone;
-                    // updateActiveZone(null);
-
+                    updateDA();
                     indicator.detach();
-
-                    // if (!(zone instanceof HTMLElement)) return;
-                    // const adr = zone.dataset.address;
-                    // if (typeof adr !== 'string') return;
-                    // console.log(Deriv.lookup(adr)?.conc);
+                    executeDA(dropAction);
                 }
             };
         },
