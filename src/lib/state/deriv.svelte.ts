b28be8d83b18653867a1a081775cce31ba8c975b
Can Baykar <togancan42@gmail.com>
2025-06-25 01:46:53 +0300

    feat(state): Add limit parameter to find

--- src/lib/state/deriv.svelte.ts
+++ src/lib/state/deriv.svelte.ts
@@ -42,9 +42,11 @@ export default class Deriv extends Parent {
 		this.render = new DerivRenderData(this, s.render ?? {});
 	}
 
-	/** Util for crawling Derivs to find the first one that satisfies fn */
-	static find(start: Deriv, fn: (d: Deriv) => boolean, backwards = false, reverse = false): Deriv | null {
-		return Crawler.find(start, fn, backwards, reverse);
+	/** Util for crawling Derivs to find the first one that satisfies fn
+	 * 	The ordering isn't symmetric so going backwards in the ordering and going forwards in the reversed ordering are different.
+	 * 	Limit is the height limit relative to current position, integer or Infinity. When it's 0 (or negative), doesn't go any higher. */
+	static find(start: Deriv, fn: (d: Deriv) => boolean, backwards = false, reverse = false, limit = Infinity): Deriv | null {
+		return Crawler.find(start, fn, backwards, reverse, limit);
 	}
 }
 
@@ -53,8 +55,12 @@ export default class Deriv extends Parent {
 export class Crawler {
 	/** Current node being crawled */
 	curr: Deriv;
-	constructor(deriv: Deriv) {
+	/** Height limit (integer or infinite) relative to curr. If 0, can't gat higher. */
+	limit: number;
+
+	constructor(deriv: Deriv, limit = Infinity) {
 		this.curr = deriv;
+		this.limit = limit;
 	}
 
 	next(reverse = false) {
@@ -70,8 +76,8 @@ export class Crawler {
 		return true;
 	}
 
-	static find(start: Deriv, fn: (d: Deriv) => boolean, backwards = false, reverse = false): Deriv | null {
-		return new Crawler(start).find(fn, backwards, reverse);
+	static find(start: Deriv, fn: (d: Deriv) => boolean, backwards = false, reverse = false, limit = Infinity): Deriv | null {
+		return new Crawler(start, limit).find(fn, backwards, reverse);
 	}
 	find(fn: (d: Deriv) => boolean, backwards = false, reverse = false): Deriv | null {
 		return (backwards ? this.findBackwards : this.findForwards).bind(this)(fn, reverse);
@@ -91,7 +97,10 @@ export class Crawler {
 	getNext(curr = this.curr, reverse = false): Deriv | null {
 		// Case: Has children
 		const firstChild = curr.children[reverse ? curr.children.length - 1 : 0];
-		if (firstChild) return firstChild;
+		if (firstChild && this.limit > 0) {
+			this.limit--;
+			return firstChild;
+		}
 		// Case: Check for right cousin (sibling counts as cuisin)
 		return this.getNextCousin(curr, reverse);
 	}
@@ -102,6 +111,7 @@ export class Crawler {
 		const rightSib = curr.derivParent.children[curr.childIndex + 1 - +reverse * 2];
 		if (rightSib) return rightSib;
 		// Case: Check right (lower) cousins of higher degree
+		this.limit++;
 		return this.getNextCousin(curr.derivParent, reverse);
 	}
 
@@ -110,14 +120,17 @@ export class Crawler {
 		// Case: No parent
 		if (!curr.derivParent) return null;
 		// Case: Is first child
-		if (curr.childIndex === (reverse ? curr.children.length - 1 : 0))
+		if (curr.childIndex === (reverse ? curr.children.length - 1 : 0)) {
+			this.limit++;
 			return curr.derivParent;
+		}
 		// Case: Return last node of left sibling
 		const leftSib = curr.derivParent.children[curr.childIndex - 1 + +reverse * 2];
 		return this.getLast(leftSib);
 	}
 	private getLast(curr = this.curr, reverse = false): Deriv | null {
-		if (!curr.children.length) return curr;
+		if (!curr.children.length || this.limit <= 0) return curr;
+		this.limit--;
 		return this.getLast(curr.children[reverse ? 0 : curr.children.length - 1]);
 	}
 }
