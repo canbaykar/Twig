2db2bdde0c3fd835861ad2e9d1dd358be9974550
Can <togancan42@gmail.com>
2025-07-11 14:21:29 +0300

    Revert "refactor(ui): Refactor deriv rendering logic in viewport to derivs.svelte"
    This reverts commit fcf275758a9aa20044940db24a55e4806bc38d29.
    
    Backgrounds will be implemented in Deriv instead. It may be renamed back to node later

--- src/lib/components/viewport/viewport.svelte
+++ src/lib/components/viewport/viewport.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { addExampleProof } from "$lib/state/deriv.svelte";
 	import type { Viewport } from "$lib/state/viewport.svelte";
+	import { onDestroy } from "svelte";
+	import Deriv from "./deriv/deriv.svelte";
+	import DerivRenderData from "./deriv/renderData.svelte";
 	import Panzoom from "./panzoom/panzoom.svelte";
 	import { mouse } from "$lib/utils/interact/mouse.svelte";
-	import Derivs from "./deriv/derivs.svelte";
     
     interface Props {
         viewport: Viewport;
@@ -13,6 +15,8 @@
 
     viewport.detachAll();
     addExampleProof();
+
+    onDestroy(() => DerivRenderData.onDestroy());
 </script>
 
 <!-- The :global(.hover) class is used to determine non-DND when hover effects should happen -->
@@ -23,7 +27,9 @@
     use:mouse.action
 >
     <Panzoom data={viewport.render}>
-        <Derivs/>
+        {#each DerivRenderData.displayed as data (data)}
+            <Deriv {data}></Deriv>
+        {/each}
         
         {#each viewport.render.panzoomPopups.children as popup (popup)}
             <popup.component {popup} {...popup.props} />
