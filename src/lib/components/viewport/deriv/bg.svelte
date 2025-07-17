971c2d301a25a6935d3bea39ee9c419d1310273a
Can <togancan42@gmail.com>
2025-07-17 20:38:08 +0300

    refactor(ui): Refactor constants to avoid recalculation
    Putting (non-constant expression)+(constant expression) in svelte reactive attribute fields causes the constant expressions to be recalculated everytime with the variable part. So I defined constants for those parts to maybe make it perform better? This may be a bit of a bad-practive but I'm not sure. Will keep it this way for now.

--- src/lib/components/viewport/deriv/bg.svelte
+++ src/lib/components/viewport/deriv/bg.svelte
@@ -1,48 +1,51 @@
 <script module lang="ts">
-    import Bg from './bg.svelte';
+	import Bg from './bg.svelte';
 	import Deriv from '$lib/state/deriv.svelte';
 
-    // Don't use the Bg component directly!
-    // Instead render bgDependency at lteast once on the page and
-    // Render bgRoot only on root nodes.
-    export { bgDependency, bgRoot };
+	// Don't use the Bg component directly!
+	// Instead render bgDependency at lteast once on the page and
+	// Render bgRoot only on root nodes.
+	export { bgDependency, bgRoot };
 
 	const saturate = 1.5;
 	const lighten = 0.05;
-    const lighten_ = 1 - lighten;
+	const lighten_ = 1 - lighten;
 
-    const matrix = `
+	const matrix = `
     ${(0.213 + 0.787 * saturate) * lighten_} ${(0.715 - 0.715 * saturate) * lighten_} ${(0.072 - 0.072 * saturate) * lighten_} 0 ${lighten}
     ${(0.213 - 0.213 * saturate) * lighten_} ${(0.715 + 0.285 * saturate) * lighten_} ${(0.072 - 0.072 * saturate) * lighten_} 0 ${lighten}
     ${(0.213 - 0.213 * saturate) * lighten_} ${(0.715 - 0.715 * saturate) * lighten_} ${(0.072 + 0.928 * saturate) * lighten_} 0 ${lighten}
     0 0 0 1 0`;
-    
-    const color = "var(--color-bg)";
-    const padding = 3 * DT.UNIT;
-    const rad = padding + 5 * DT.UNIT;
+
+	const color = 'var(--color-bg)';
+
+	const pad = 3 * DT.UNIT;
+	
+	// Helper constants for bg dimensions are here to prevent being recalculated
+	// all the time
+	const pad2 = 2 * pad;
+	
+    const formulaOffY = -DT.derivLineHeightN - pad;
+	const formulaBgHeight = DT.derivLineHeightN + pad2;
+	const formulaRx = $derived(
+		Math.min((pad + 5 * DT.UNIT) / viewport.render.scale, formulaBgHeight / 2)
+	);
+
+    const barOffY = -DT.derivBarBottomN - pad - DT.UNIT;
+    const barBgHeight = DT.UNIT + pad2;
+    const barRx = barBgHeight / 2;
+
+    const labelOffX = -DT.derivBarGapN - pad;
+    const labelOffY = -DT.derivBarBottomN + DT.derivLabelBottomN - DT.UNIT - pad;
+    const labelBgHeight = DT.derivLabelHeightN + pad2;
+    const labelRx = DT.derivLabelHeightN / 2 + pad;
+
+	const ruleLabelOffX = DT.derivBarGapN - pad;
+
+    const ruleOffX = DT.derivRuleLeftN + DT.derivRuleParanthesisGapN - pad;
+    const ruleOffY = -DT.derivBarBottomN + DT.derivRuleBottomN + (DT.derivRuleHeightN - DT.derivLabelHeightN) / 2 - DT.UNIT - pad;
+	const rulePad = 2 * (pad - DT.derivRuleParanthesisGapN);
 </script>
-
-{#snippet bgDependency()}
-    <svg class=" absolute invisible">
-		<filter id="outlineFilter" color-interpolation-filters="sRGB">
-            <feColorMatrix
-            in="SourceGraphic"
-            result="lightened"
-            type="matrix"
-            values={matrix} />
-			<feMorphology operator="dilate" in="lightened" result="morphed" radius={1.1 * DT.UNIT / viewport.render.scale}/>
-            <feComposite in="SourceGraphic" />
-		</filter>
-	</svg>
-{/snippet}
-
-{#snippet bgRoot(data: Deriv)}
-    <svg class="pointer-events-none h-[1px] w-[1px] overflow-visible opacity-100" viewBox="0 0 1 1">
-        <g id="g" filter="url(#outlineFilter)" fill={color}>
-			<Bg {data} />
-		</g>
-    </svg>
-{/snippet}
 
 <script lang="ts">
 	import { DT } from '../../../../DT';
@@ -55,59 +58,82 @@
 	let { data }: Props = $props();
 </script>
 
+{#snippet bgDependency()}
+	<svg class=" invisible absolute">
+		<filter id="outlineFilter" color-interpolation-filters="sRGB">
+			<feColorMatrix in="SourceGraphic" result="lightened" type="matrix" values={matrix} />
+			<feMorphology
+				operator="dilate"
+				in="lightened"
+				result="morphed"
+				radius={(1.1 * DT.UNIT) / viewport.render.scale}
+			/>
+			<feComposite in="SourceGraphic" />
+		</filter>
+	</svg>
+{/snippet}
+
+{#snippet bgRoot(data: Deriv)}
+	<svg class="pointer-events-none h-[1px] w-[1px] overflow-visible opacity-100" viewBox="0 0 1 1">
+		<g id="g" filter="url(#outlineFilter)" fill={color}>
+			<Bg {data} />
+		</g>
+	</svg>
+{/snippet}
+
 <!-- For formula -->
 <rect
-    x={data.render.x - data.render.width / 2 - padding}
-    y={data.render.y - DT.derivLineHeightN - padding}
-    width={data.render.width + 2 * padding}
-    height={DT.derivLineHeightN + 2 * padding}
-    rx={Math.min(rad / viewport.render.scale, (DT.derivLineHeightN + 2 * padding) / 2)}
+	x={data.render.x - data.render.width / 2 - pad}
+	y={data.render.y + formulaOffY}
+	width={data.render.width + pad2}
+	height={formulaBgHeight}
+	rx={formulaRx}
 />
 
 <!-- Recursion for children -->
 {#each data.children as child (child)}
-    <Bg data={child} />
+	<Bg data={child} />
 {/each}
 
 <!-- For bar -->
 {#if data.render.hasBar}
-    <rect
-        x={data.render.x - data.render.barWidth / 2 - padding}
-        y={data.render.y - DT.derivBarBottomN - padding - DT.UNIT}
-        width={data.render.barWidth + 2 * padding}
-        height={DT.UNIT + 2 * padding}
-        rx={(DT.UNIT + 2 * padding) / 2}
-    />
-    
-    <!-- For label -->
-    {#if data.render.hasLabel}
-        <rect
-            x={data.render.x - DT.derivBarGapN - data.render.barWidth / 2 - data.render.labelWidth - padding}
-            y={data.render.y - DT.derivBarBottomN + DT.derivLabelBottomN - DT.UNIT - padding}
-            width={data.render.labelWidth + 2 * padding}
-            height={DT.derivLabelHeightN + 2 * padding}
-            rx={DT.derivLabelHeightN / 2 + padding}
-        />
-    {/if}
+	<rect
+		x={data.render.x - data.render.barWidth / 2 - pad}
+		y={data.render.y + barOffY}
+		width={data.render.barWidth + pad2}
+		height={barBgHeight}
+		rx={barRx}
+	/>
+
+	<!-- For label -->
+	{#if data.render.hasLabel}
+		<rect
+			x={data.render.x - data.render.barWidth / 2 - data.render.labelWidth + labelOffX}
+			y={data.render.y + labelOffY}
+			width={data.render.labelWidth + pad2}
+			height={labelBgHeight}
+			rx={labelRx}
+		/>
+	{/if}
 
 	<!-- For Rule -->
-    {#if data.render.hasRule}
-        {#if data.render.discharged}
-            <rect
-                x={data.render.x + DT.derivBarGapN + data.render.barWidth / 2 - padding}
-                y={data.render.y - DT.derivBarBottomN + DT.derivLabelBottomN - DT.UNIT - padding}
-                width={data.render.ruleWidth + 2 * padding}
-                height={DT.derivLabelHeightN + 2 * padding}
-                rx={DT.derivLabelHeightN / 2 + padding}
-            />
-        {:else}
-            <rect
-                x={data.render.x + DT.derivRuleLeftN + data.render.barWidth / 2 + DT.derivRuleParanthesisGapN - padding}
-                y={data.render.y - DT.derivBarBottomN + DT.derivRuleBottomN + (DT.derivRuleHeightN - DT.derivLabelHeightN) / 2 - DT.UNIT - padding}
-                width={data.render.ruleWidth + 2 * (padding - DT.derivRuleParanthesisGapN)}
-                height={DT.derivLabelHeightN + 2 * padding}
-                rx={DT.derivLabelHeightN / 2 + padding}
-            />
-        {/if}
-    {/if}
+	{#if data.render.hasRule}
+		{#if data.render.discharged}
+			<rect
+				x={data.render.x + data.render.barWidth / 2 + ruleLabelOffX}
+				y={data.render.y + labelOffY}
+				width={data.render.ruleWidth + pad2}
+				height={labelBgHeight}
+				rx={labelRx}
+			/>
+		{:else}
+			<rect
+				x={data.render.x + data.render.barWidth / 2 + ruleOffX}
+				y={data.render.y + ruleOffY}
+				width={data.render.ruleWidth + rulePad}
+				height={labelBgHeight}
+				rx={labelRx}
+			/>
+		{/if}
+	{/if}
 {/if}
