2d30defbb44dd4c7fd719099f7dd478f6388646b
Can Baykar <togancan42@gmail.com>
2025-08-07 16:03:46 +0300

    style(ui): Remove commented out code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,6 @@
 	import './global.css';
 	import ViewportC from "$lib/components/viewport/viewportC.svelte";
 	import viewport from "$lib/state/viewport.svelte";
-
-	// import grammar from "$lib/state/logic/grammar";
-	// console.clear();
-	// const parsed = grammar.parse(' (A∧B)→[0] ');
-	// console.log(parsed + '');
-	// console.log(JSON.stringify(parsed, null, 2));
 </script>
 
 <ViewportC {viewport} />
