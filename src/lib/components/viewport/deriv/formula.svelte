9061aaa43d6aa9498495a077cff8b66799bd6da0
Can Baykar <togancan42@gmail.com>
2025-08-08 13:06:10 +0300

    refactor(ui): Editor enabling-disabling logic replaced by hovered-selected system
    This was a part of the plan with that system from start. To gather most listeners in viewport. This commit is one case where that simplifies things.

--- src/lib/components/viewport/deriv/formula.svelte
+++ src/lib/components/viewport/deriv/formula.svelte
@@ -25,63 +25,18 @@
 	// or when it's in focus. It's is slightly mispositioned due to rounding, so it's
 	// not the main element that displays the text but just an overlay for caret.
 	// (But it also displays selection text.)
-	let editorElement: HTMLElement | null = $state(null);
-
-	let editorEnabled = $state(false);
-	const enableEditor = () => (editorEnabled = true);
-	const disableEditor = () => (editorEnabled = false);
-
-	let draggedOver = false;
-	function ondragenter() {
-		enableEditor();
-		draggedOver = true;
-	}
-	function ondragleave() {
-		draggedOver = false;
-	}
-
-	function onmouseleave(e: MouseEvent) {
-		if (
-			draggedOver ||
-			element?.contains?.(e.relatedTarget as Node | null) ||
-			element?.contains?.(document.activeElement)
-		)
-			return;
-		disableEditor();
-	}
-	async function onfocus() {
-		enableEditor();
-		// await tick();
-		// // Do nothing if focus changed while awaiting
-		// if (document.activeElement !== element) return;
-		// editorElement?.focus?.();
-	}
-	function onblur() {
-		if (!element?.matches?.(':hover')) disableEditor();
-	}
-
-	// For debugging
-	function ondblclick() { // @ts-ignore
-		console.log(data.render.tree);
-	}
 </script>
 
 <!-- Draggable button, grabbed when clicked -->
 <button
 	class="bottom-0 translate-x-[-50%] cursor-default px-(--DERIV-X-PADDING) leading-(--DERIV-LINE-HEIGHT) h-(--DERIV-LINE-HEIGHT) select-none whitespace-nowrap"
 	bind:this={element}
-	onmouseenter={enableEditor}
-	{ondragenter}
-	{ondragleave}
-	{onmouseleave}
-	{onfocus}
-	{ondblclick}
 	tabindex="-1"
 >
 	<span class="contents whitespace-pre">{data.conc}</span>
 
 	<!-- EDITOR -->
-	{#if editorEnabled}
+	{#if data.render.hovered || data.render.selected}
 		<textarea
 			bind:value={data.conc}
 			class="caret-fg absolute top-0 left-0 origin-top-left resize-none overflow-hidden text-center text-transparent"
@@ -91,8 +46,6 @@
             font-size: {DT.derivSizeN / DT.UNIT * viewport.render.scale}px;
             scale: {DT.UNIT / viewport.render.scale};
         "
-			bind:this={editorElement}
-			{onblur}
 			rows="1"
 			data-part="formula"
 		></textarea>
