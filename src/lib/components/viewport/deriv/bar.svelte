e0a85945258e988d9ed023fe089053aa32308ca7
Can Baykar <togancan42@gmail.com>
2025-08-24 22:02:30 +0300

    feat(ui): Make bars of non-infered derivs muted

--- src/lib/components/viewport/deriv/bar.svelte
+++ src/lib/components/viewport/deriv/bar.svelte
@@ -31,7 +31,7 @@
 </script>
 
 <div
-    class="bg-fg h-1 origin-left translate-x-[-50%] rounded-full select-none cursor-all-scroll"
+    class="{render.inferred ? "bg-fg" : "bg-fg/25"} h-1 origin-left translate-x-[-50%] rounded-full select-none cursor-all-scroll"
 	style:translate="{data.render.xBar - data.render.barWidth / 2}px {data.render.yBar}px"
     style:width="{render.barWidth}px"
     class:bg-transparent={data.render.barHidden}
