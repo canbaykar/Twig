fa3992a8773b3211c4144357c3d8b035cae79bf6
Can Baykar <togancan42@gmail.com>
2025-09-07 21:02:27 +0300

    refactor(ui): Turn formula editor field to contenteditable (from textarea)
    I have to implement a simultaneous multiple editing/selection feature like in code editors because top and bottom adders require it. Making virtual caret and selection with the method Muffin Man used on his curstom giraffe caret was plan A but that messed up kerning and ligatures and I really don't want to yield to monospace soo Prosemirror is the way to go. Starting off by making formula contenteditable while preserving most style.

--- src/lib/components/viewport/deriv/formula.svelte
+++ src/lib/components/viewport/deriv/formula.svelte
@@ -20,47 +20,39 @@
 		() => deriv.conc,
 		() => deriv.render.baseWidth = element.offsetWidth
 	);
-
-	// The "element" can't be used as an input field bc the caret may be too thin on
-	// some browser due to how pazoom transforms the element. :(
-	// So we have an "editor" element with transparent text that only exists on hover
-	// or when it's in focus. It's is slightly mispositioned due to rounding, so it's
-	// not the main element that displays the text but just an overlay for caret.
-	// (But it also displays selection text.)
 </script>
 
 <!-- Draggable button, grabbed when clicked -->
-<button
-	class="bottom-0 translate-x-[-50%] cursor-default px-(--DERIV-X-PADDING) leading-(--DERIV-LINE-HEIGHT) h-(--DERIV-LINE-HEIGHT) select-none whitespace-nowrap"
+<div
+	class="wrapper bottom-0 translate-x-[-50%] h-(--DERIV-LINE-HEIGHT) leading-(--DERIV-LINE-HEIGHT) whitespace-nowrap cursor-auto"
 	bind:this={element}
 	tabindex="-1"
 >
-	<!-- For some reason this element's slightly taller than expected so we need pointer-events-none to avoid covering bars -->
-	<span 
-		class="relative! whitespace-pre pointer-events-none"
+	<div 
+		contenteditable
+		bind:innerText={deriv.conc}
+		class="relative! inline-block whitespace-pre px-(--DERIV-X-PADDING) overflow-hidden outline-hidden"
 		class:opacity-25={deriv.render.bodyMuted}
-	>{deriv.conc}</span>
-
-	<!-- EDITOR -->
-	<!-- TODO: Outline & border radius styling code too unorganised -->
+		data-part="body_formula"
+	></div>
 	{#if deriv.render.bodyAwake}
-		<textarea
-			bind:value={deriv.conc}
-			class="
-				caret-fg absolute top-0 left-0 origin-top-left resize-none overflow-hidden text-center text-transparent
-				outline-0 outline-fg outline-dashed focus:outline-1 -outline-offset-1 rounded-[calc(var(--DERIV-BG-PADDING)/16)]
-			"
+		<div
+			class="outline_ opacity-0 top-0 left-0 origin-top-left pointer-events-none outline-1 outline-fg -outline-offset-1 outline-dashed rounded-[calc(var(--DERIV-BG-PADDING)/16)]"
 			style="
-            line-height: {DT.derivLineHeightN / DT.UNIT * viewport.render.scale}px;
-            width: {deriv.render.baseWidth / DT.UNIT * viewport.render.scale}px;
-            font-size: {DT.derivSizeN / DT.UNIT * viewport.render.scale}px;
-            scale: {DT.UNIT / viewport.render.scale};
-        "
-			rows="1"
-			data-part="body_formula"
-		></textarea>
+				height: {DT.derivLineHeightN / DT.UNIT * viewport.render.scale}px;
+				width: {deriv.render.baseWidth / DT.UNIT * viewport.render.scale}px;
+				font-size: {DT.derivSizeN / DT.UNIT * viewport.render.scale}px;
+				scale: {DT.UNIT / viewport.render.scale};
+        	"
+		></div>
 	{/if}
-</button>
+</div>
+
+<style>
+	.wrapper:focus-within .outline_ {
+		opacity: 1;
+	}
+</style>
 
 <!-- BG -->
 {#snippet formulaBg(deriv: Deriv, type: BgType)}
