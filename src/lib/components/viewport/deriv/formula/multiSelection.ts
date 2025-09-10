d50258e23ba9946a028f757ae87df066625ef02a
Can <togancan42@gmail.com>
2025-09-10 16:32:05 +0300

    feat(ui): Setup Alt+Select* & Left/RightArrow for prosemirror
    Alt+Click to remove selection still not done.

--- src/lib/components/viewport/deriv/formula/multiSelection.ts
+++ src/lib/components/viewport/deriv/formula/multiSelection.ts
@@ -1,6 +1,7 @@
 import { Slice, type Node, type ResolvedPos } from "prosemirror-model";
 import { Plugin, Selection, TextSelection, Transaction } from "prosemirror-state";
 import type { Mappable } from "prosemirror-transform";
+import type { EditorView } from "prosemirror-view";
 
 const jsonID = 'multi';
 
@@ -9,12 +10,22 @@ export class MultiSelection extends Selection {
 	readonly main: Selection;
 	/** Number of selections */
 	get length() { return this.selections.length; }
+	/** Index of main */
+	get mainIndex() { return this.selections.indexOf(this.main); }
 
-	constructor(selections: Selection[], main = selections[0]) {
-		if (!main) throw new Error("Invalid arguments for MultiSelection. Main selection doesn't exist.");
-		// Normalise array
+	constructor(selections: Selection[], main = selections[selections.length - 1]) {
+		if (!main) 
+			throw new Error("Invalid arguments for MultiSelection. Main selection doesn't exist.");
+		
+		// Normalise main
+		while (main instanceof MultiSelection) main = main.main;
+
+		// Normalise selections
 		selections = selections
-			.map(s => s instanceof MultiSelection ? s.selections : s).flat()
+			.map(s => s instanceof MultiSelection ? s.selections : s).flat();
+		if (!selections.includes(main)) 
+			throw new Error("Invalid arguments for MultiSelection. Main selection isn't in selections.");
+		selections = selections
 			.sort((a, b) => a.from - b.from)
 			// Handles conflicts (remove if intersecting, merge ones in the same place, etc.)
 			.reduce((acc: Selection[], curr) => {
@@ -30,9 +41,10 @@ export class MultiSelection extends Selection {
 					return acc;
 				}
 				if (prev.to === curr.from) return [...acc, curr];
-				if (prev === main) main = curr;
+				if (prev === main) return acc;
 				return [...acc.slice(0, acc.length - 2), curr];
 			}, []);
+		
 		const ranges = selections.map(s => s.ranges).flat();
 		super(main.$anchor, main.$head, ranges);
 		this.selections = selections;
@@ -64,24 +76,24 @@ export class MultiSelection extends Selection {
 	}
 
 	replace(tr: Transaction, content?: Slice): void {
-		const newS: Selection[] = [];
-		let newMain: Selection | undefined = undefined;
+		let newS: Selection[] = [];
 		for (const sel of this.selections) {
-			sel.replace(tr, content);
+			const sel_ = sel.map(tr.doc, tr.mapping);
+			sel_.replace(tr, content);
 			newS.push(tr.selection);
-			if (sel === this.main) newMain = tr.selection;
 		}
-		tr.setSelection(new MultiSelection(newS, newMain));
+		// newS = newS.map(s => s.map(tr.doc, tr.mapping));
+		tr.setSelection(new MultiSelection(newS, newS[this.mainIndex]));
 	}
 	replaceWith(tr: Transaction, node: Node): void {
-		const newS: Selection[] = [];
-		let newMain: Selection | undefined = undefined;
+		let newS: Selection[] = [];
 		for (const sel of this.selections) {
-			sel.replaceWith(tr, node);
+			const sel_ = sel.map(tr.doc, tr.mapping);
+			sel_.replaceWith(tr, node);
 			newS.push(tr.selection);
-			if (sel === this.main) newMain = tr.selection;
 		}
-		tr.setSelection(new MultiSelection(newS, newMain));
+		// newS = newS.map(s => s.map(tr.doc, tr.mapping));
+		tr.setSelection(new MultiSelection(newS, newS[this.mainIndex]));
 	}
 
 	toJSON() {
@@ -104,16 +116,53 @@ export class MultiSelection extends Selection {
 		const offsetHead = dir < 0 
 			? (sel: Selection) => tr.doc.resolve(Math.max(sel.head - 1, 0))
 			: (sel: Selection) => tr.doc.resolve(Math.min(sel.head + 1, tr.doc.content.size));
-		const newS: Selection[] = [];
-		let newMain: Selection | undefined = undefined;
+		
+		let newS: Selection[] = [];
 		for (const sel of this.selections) {
-			if (sel.empty)
-				TextSelection.between(sel.$anchor, offsetHead(sel)).replace(tr, Slice.empty);
-			else sel.replace(tr, Slice.empty);
+			const sel_ = sel.map(tr.doc, tr.mapping);
+			if (sel_.empty)
+				TextSelection.between(sel_.$anchor, offsetHead(sel_))
+					.replace(tr, Slice.empty);
+			else sel_.replace(tr, Slice.empty);
 			newS.push(tr.selection);
-			if (sel === this.main) newMain = tr.selection;
 		}
-		tr.setSelection(new MultiSelection(newS, newMain));
+		// newS = newS.map(s => s.map(tr.doc, tr.mapping));
+		tr.setSelection(new MultiSelection(newS, newS[this.mainIndex]));
+	}
+
+	/** Add s2 (as main) to s1 and return the resulting MultiSelection */
+	static withAdded(s1: Selection, s2: Selection) {
+		return new MultiSelection([s1, s2]);
+	}
+
+	/** If s isn't an instance of MultiSelection, return newMain. 
+	 *  Else return s with it's main selection thrown out and replaced with newMain */
+	static withReplacedMain(s: Selection, newMain: Selection) {
+		if (!(s instanceof MultiSelection)) return newMain;
+		const selections = [...s.selections];
+		selections.splice(selections.indexOf(s.main), 1, newMain);
+		return new MultiSelection(selections, newMain);
+	}
+
+	/** Apply left(dir = -1) or right(dir = 1) arrow press. Accounts for Shift+Arrow but
+	 *  not Ctrl+Arrow and assumes all selections are text :(. For mod argument refer to 
+	 * 	getMods from prosemirror-view/srv/capturekeys.ts */
+	move(tr: Transaction, dir: number, mods: string) {
+		const offsetHead = dir < 0 
+			? (sel: Selection) => tr.doc.resolve(Math.max(sel.head - 1, 0))
+			: (sel: Selection) => tr.doc.resolve(Math.min(sel.head + 1, tr.doc.content.size));
+		// Assumes s is TextSelection
+		const toMoved = mods.indexOf('s') > -1 
+			? (s: Selection) => TextSelection.between(s.$anchor, offsetHead(s))
+			: (s: Selection) => {
+				const $h = s.empty ? (offsetHead(s)) : (dir < 0 ? s.$to : s.$from);
+				return TextSelection.between($h, $h);
+			};
+		let newS: Selection[] = [];
+		for (const sel of this.selections) {
+			newS.push(toMoved(sel));
+		}
+		tr.setSelection(new MultiSelection(newS, newS[this.mainIndex]));
 	}
 }
 
@@ -122,7 +171,7 @@ export class MultiSelection extends Selection {
 // Register jsonID
 // Selection.jsonID(jsonID, MultiSelection); //////////////////////////////////////////////////////////////////////////
 
-// Copied from prosemirror-view (browser.ts and capturekeys.ts) for handleKeyDown below
+// == Copied from prosemirror-view (browser.ts and capturekeys.ts) for handleKeyDown below ==
 const nav = typeof navigator != "undefined" ? navigator : null;
 const agent = (nav && nav.userAgent) || "";
 const ie_edge = /Edge\/(\d+)/.exec(agent);
@@ -132,6 +181,29 @@ const ie = !!(ie_upto10 || ie_11up || ie_edge);
 const safari = !ie && !!nav && /Apple Computer/.test(nav.vendor);
 const ios = safari && (/Mobile\/\w+/.test(agent) || !!nav && nav.maxTouchPoints > 2);
 const mac = ios || (nav ? /Mac/.test(nav.platform) : false);
+const _chrome = !ie && /Chrome\/(\d+)/.exec(agent);
+const chrome = !!_chrome;
+const windows = nav ? /Win/.test(nav.platform) : false;
+function findDirection(view: EditorView, pos: number): "rtl" | "ltr" {
+  let $pos = view.state.doc.resolve(pos)
+  if (!(chrome || windows) && $pos.parent.inlineContent) {
+    let coords = view.coordsAtPos(pos)
+    if (pos > $pos.start()) {
+      let before = view.coordsAtPos(pos - 1)
+      let mid = (before.top + before.bottom) / 2
+      if (mid > coords.top && mid < coords.bottom && Math.abs(before.left - coords.left) > 1)
+        return before.left < coords.left ? "ltr" : "rtl"
+    }
+    if (pos < $pos.end()) {
+      let after = view.coordsAtPos(pos + 1)
+      let mid = (after.top + after.bottom) / 2
+      if (mid > coords.top && mid < coords.bottom && Math.abs(after.left - coords.left) > 1)
+        return after.left > coords.left ? "ltr" : "rtl"
+    }
+  }
+  let computed = getComputedStyle(view.dom).direction
+  return computed == "rtl" ? "rtl" : "ltr"
+}
 function getMods(event: KeyboardEvent) {
   let result = ""
   if (event.ctrlKey) result += "c"
@@ -140,62 +212,84 @@ function getMods(event: KeyboardEvent) {
   if (event.shiftKey) result += "s"
   return result
 }
+// == End of copied dependencies ==
+
+/** Selection state preserved while holding alt */
+type AltState = {
+	altKey: boolean;
+	fresh: boolean; // mousedown fired but selection didn't update yet
+	deletionMode: boolean; // selection updated and deleted a selection but didn't update yet after that
+};
+const altUp = { altKey: false, fresh: false, deletionMode: false };
+function getAltState(view: EditorView): AltState {
+	return multiSelectionPlugin.getState(view.state);
+}
+function setAltState(view: EditorView, val: AltState) {
+	view.dispatch(view.state.tr.setMeta(multiSelectionPlugin, val));
+}
 
 export const multiSelectionPlugin: Plugin = new Plugin({
-	// Holds altKey state
 	state: {
-		init() {
-			return false;
+		init(): AltState {
+			return altUp;
 		},
-		apply(tr, value) {
-			return tr.getMeta(multiSelectionPlugin)?.altKey ?? value;
+		apply(tr, value): AltState {
+			if (tr.getMeta(multiSelectionPlugin) ?? value !== value) console.log(tr.getMeta(multiSelectionPlugin))
+			return tr.getMeta(multiSelectionPlugin) ?? value;
 		},
 	},
 
 	props: {
+		handleDOMEvents: {
+			mousedown(view, e) {
+				if (e.altKey) 
+					setAltState(view, { altKey: true, fresh: true, deletionMode: false });
+			},
+			keyup(view, e) {
+				if (e.key === 'Alt') setAltState(view, altUp);
+			}
+		},
+
 		createSelectionBetween(view, $anchor, $head) {
-			// Default behaviour:
-			// return TextSelection.between($anchor, $head);
-			return MultiSelection.between($anchor, $head);
+			const altState = getAltState(view);
+			const newS = TextSelection.between($anchor, $head);
+			if (!altState.altKey)
+				return newS; // Default behaviour
+
+			if (altState.fresh) return MultiSelection.withAdded(view.state.selection, newS);
+			else return MultiSelection.withReplacedMain(view.state.selection, newS);
 		},
 
 		handleKeyDown(view, e) {
-			// Send altKey state
-			const key = e.key
-			if (key == 'Alt') {
-				view.dispatch(view.state.tr.setMeta(multiSelectionPlugin, { altKey: true }));
-				return;
-			}
-			
 			const state = view.state;
 			const sel = state.selection;
-
+			
 			if (!(sel instanceof MultiSelection)) return;
-
+			
 			// This function has some copied logic from prosemirror-view/src/capturekeys
-			const mods = getMods(e);
-			if (key == 'Backspace' || (mac && key == 'h' && mods == "c")) {
+			// so if confused, see that.
+			const key = e.key, mods = getMods(e), tr = state.tr;
+			if (key === 'Backspace' || (mac && key == 'h' && mods == "c")) {
 				// Backspace, Ctrl-h on Mac
-				const tr = state.tr;
 				sel.delete(tr, -1);
 				view.dispatch(tr);
-			} else if ((key == 'Delete' && !e.shiftKey) || (mac && key == 'd' && mods == "c")) {
+				return true;
+			} else if ((key === 'Delete' && !e.shiftKey) || (mac && key == 'd' && mods == "c")) {
 				// Delete, Ctrl-d on Mac
-				const tr = state.tr;
 				sel.delete(tr, 1);
 				view.dispatch(tr);
+				return true;
+			} else if (key === 'ArrowLeft' || (mac && key === 'b' && mods == "c")) {
+    			let dir = key === 'ArrowLeft' ? (findDirection(view, view.state.selection.from) == "ltr" ? -1 : 1) : -1;
+				sel.move(tr, dir, mods);
+				view.dispatch(tr);
+				return true;
+			} else if (key === 'ArrowRight' || (mac && key === 'f' && mods == "c")) {
+				let dir = key === 'ArrowRight' ? (findDirection(view, view.state.selection.from) == "ltr" ? -1 : 1) : -1;
+				sel.move(tr, dir, mods);
+				view.dispatch(tr);
+				return true;
 			}
-			return;
-		},
-
-		handleDOMEvents: {
-			keyup(view, e) {
-				// Send altKey state
-				const key = e.key
-				if (key == 'Alt')
-					view.dispatch(view.state.tr.setMeta(multiSelectionPlugin, { altKey: false }));
-				return;
-			},
 		},
 	},
 });
