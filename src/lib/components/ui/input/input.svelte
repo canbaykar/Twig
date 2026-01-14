3d0582e4245bcaab6c5e37d16555c48c29f15e41
Can Baykar <togancan42@gmail.com>
2026-01-15 00:20:58 +0300

    refactor(ui): Fix input colors
    Previously input was just imported from shadcn without any changes.

--- src/lib/components/ui/input/input.svelte
+++ src/lib/components/ui/input/input.svelte
@@ -25,9 +25,9 @@
 		bind:this={ref}
 		data-slot={dataSlot}
 		class={cn(
-			"selection:bg-primary dark:bg-input/30 selection:text-primary-foreground border-input ring-offset-background placeholder:text-muted-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 pt-1.5 text-sm font-medium shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
-			"focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-			"aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+			"selection:bg-control-fg selection:text-control-bg border-b-control-border ring-offset-bg placeholder:text-control-fg-muted flex h-9 w-full min-w-0 rounded-md border bg-control-bg px-3 pt-1.5 text-sm font-medium shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
+			"focus-visible:border-focus-outline focus-visible:ring-focus-outline/50 focus-visible:ring-[3px]",
+			"aria-invalid:ring-border-danger-emphasis/20 aria-invalid:border-border-danger-emphasis",
 			className
 		)}
 		type="file"
@@ -40,9 +40,9 @@
 		bind:this={ref}
 		data-slot={dataSlot}
 		class={cn(
-			"border-input bg-background selection:bg-primary dark:bg-input/30 selection:text-primary-foreground ring-offset-background placeholder:text-muted-foreground flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-			"focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-			"aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+			"border-b-control-border bg-control-bg selection:bg-control-fg selection:text-control-bg ring-offset-bg placeholder:text-control-fg-muted flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+			"focus-visible:border-focus-outline focus-visible:ring-focus-outline/50 focus-visible:ring-[3px]",
+			"aria-invalid:ring-border-danger-emphasis/20 aria-invalid:border-border-danger-emphasis",
 			className
 		)}
 		{type}
