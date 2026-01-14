3f24f0023b7af8030057c263b4163035b9aca458
Can Baykar <togancan42@gmail.com>
2026-01-14 03:14:49 +0300

    refactor(ui): Set final menubar items minus functionality
    Replaced placeholders.

--- src/lib/components/menubar/menubar.svelte
+++ src/lib/components/menubar/menubar.svelte
@@ -1,95 +1,91 @@
 <script>
 	import * as Menubar from '$lib/components/ui/menubar';
-
-	// Temporary example menu
-	let bookmarks = $state(false);
-	let fullUrls = $state(true);
-	let profileRadioValue = $state('benoit');
+	import Icon from '@iconify/svelte';
 </script>
 
 <Menubar.Root class="absolute border-t-0 border-l-0 rounded-none rounded-br-md">
 	<Menubar.Menu>
 		<Menubar.Trigger>File</Menubar.Trigger>
 		<Menubar.Content>
-			<Menubar.Item>
-				New Tab <Menubar.Shortcut>⌘T</Menubar.Shortcut>
-			</Menubar.Item>
-			<Menubar.Item>
-				New Window <Menubar.Shortcut>⌘N</Menubar.Shortcut>
-			</Menubar.Item>
-			<Menubar.Item>New Incognito Window</Menubar.Item>
+			<Menubar.IconItem>
+				{#snippet icon()}
+					<Icon icon="lucide:file" />
+				{/snippet}
+				New File
+			</Menubar.IconItem>
+			<Menubar.IconItem>
+				{#snippet icon()}
+					<Icon icon="lucide:folder" />
+				{/snippet}
+				Open
+			</Menubar.IconItem>
 			<Menubar.Separator />
-			<Menubar.Sub>
-				<Menubar.SubTrigger>Share</Menubar.SubTrigger>
-				<Menubar.SubContent>
-					<Menubar.Item>Email link</Menubar.Item>
-					<Menubar.Item>Messages</Menubar.Item>
-					<Menubar.Item>Notes</Menubar.Item>
-				</Menubar.SubContent>
-			</Menubar.Sub>
-			<Menubar.Separator />
-			<Menubar.Item>
-				Print... <Menubar.Shortcut>⌘P</Menubar.Shortcut>
-			</Menubar.Item>
+			<Menubar.IconItem>
+				{#snippet icon()}
+					<Icon icon="lucide:save" />
+				{/snippet}
+				Save
+			</Menubar.IconItem>
+			<Menubar.IconItem>
+				{#snippet icon()}
+					<Icon icon="lucide:save" />
+				{/snippet}
+				Save As…
+			</Menubar.IconItem>
 		</Menubar.Content>
 	</Menubar.Menu>
 	<Menubar.Menu>
 		<Menubar.Trigger>Edit</Menubar.Trigger>
 		<Menubar.Content>
-			<Menubar.Item>
-				Undo <Menubar.Shortcut>⌘Z</Menubar.Shortcut>
-			</Menubar.Item>
-			<Menubar.Item disabled>
-				Redo <Menubar.Shortcut>⇧⌘Z</Menubar.Shortcut>
-			</Menubar.Item>
+			<Menubar.IconItem disabled>
+				{#snippet icon()}
+					<Icon icon="lucide:undo" />
+				{/snippet}
+				Undo <Menubar.Shortcut>(WIP)</Menubar.Shortcut>
+			</Menubar.IconItem>
+			<Menubar.IconItem disabled>
+				{#snippet icon()}
+					<Icon icon="lucide:redo" />
+				{/snippet}
+				Redo <Menubar.Shortcut>(WIP)</Menubar.Shortcut>
+			</Menubar.IconItem>
 			<Menubar.Separator />
-			<Menubar.Sub>
-				<Menubar.SubTrigger>Find</Menubar.SubTrigger>
-				<Menubar.SubContent>
-					<Menubar.Item>Search the web</Menubar.Item>
-					<Menubar.Separator />
-					<Menubar.Item>Find...</Menubar.Item>
-					<Menubar.Item>Find Next</Menubar.Item>
-					<Menubar.Item>Find Previous</Menubar.Item>
-				</Menubar.SubContent>
-			</Menubar.Sub>
+			<Menubar.IconItem disabled>
+				{#snippet icon()}
+					<Icon icon="lucide:search" />
+				{/snippet}
+				Find <Menubar.Shortcut>(WIP)</Menubar.Shortcut>
+			</Menubar.IconItem>
 			<Menubar.Separator />
-			<Menubar.Item>Cut</Menubar.Item>
-			<Menubar.Item>Copy</Menubar.Item>
-			<Menubar.Item>Paste</Menubar.Item>
+			<Menubar.IconItem>
+				{#snippet icon()}
+					<Icon icon="lucide:scissors" />
+				{/snippet}
+				Cut
+			</Menubar.IconItem>
+			<Menubar.IconItem>
+				{#snippet icon()}
+					<Icon icon="lucide:clipboard-copy" />
+				{/snippet}
+				Copy
+			</Menubar.IconItem>
+			<Menubar.IconItem>
+				{#snippet icon()}
+					<Icon icon="lucide:clipboard-paste" />
+				{/snippet}
+				Paste
+			</Menubar.IconItem>
 		</Menubar.Content>
 	</Menubar.Menu>
 	<Menubar.Menu>
-		<Menubar.Trigger>View</Menubar.Trigger>
+		<Menubar.Trigger>Help</Menubar.Trigger>
 		<Menubar.Content>
-			<Menubar.CheckboxItem bind:checked={bookmarks}>Always Show Bookmarks Bar</Menubar.CheckboxItem
-			>
-			<Menubar.CheckboxItem bind:checked={fullUrls}>Always Show Full URLs</Menubar.CheckboxItem>
-			<Menubar.Separator />
-			<Menubar.Item inset>
-				Reload <Menubar.Shortcut>⌘R</Menubar.Shortcut>
-			</Menubar.Item>
-			<Menubar.Item inset variant="destructive">
-				Force Reload <Menubar.Shortcut>⇧⌘R</Menubar.Shortcut>
-			</Menubar.Item>
-			<Menubar.Separator />
-			<Menubar.Item inset>Toggle Fullscreen</Menubar.Item>
-			<Menubar.Separator />
-			<Menubar.Item inset>Hide Sidebar</Menubar.Item>
-		</Menubar.Content>
-	</Menubar.Menu>
-	<Menubar.Menu>
-		<Menubar.Trigger>Profiles</Menubar.Trigger>
-		<Menubar.Content>
-			<Menubar.RadioGroup bind:value={profileRadioValue}>
-				<Menubar.RadioItem value="andy">Andy</Menubar.RadioItem>
-				<Menubar.RadioItem value="benoit">Benoit</Menubar.RadioItem>
-				<Menubar.RadioItem value="Luis">Luis</Menubar.RadioItem>
-			</Menubar.RadioGroup>
-			<Menubar.Separator />
-			<Menubar.Item inset>Edit...</Menubar.Item>
-			<Menubar.Separator />
-			<Menubar.Item inset>Add Profile...</Menubar.Item>
+			<Menubar.IconItem>
+				{#snippet icon()}
+					<Icon icon="lucide:github" />
+				{/snippet}
+				GitHub
+			</Menubar.IconItem>
 		</Menubar.Content>
 	</Menubar.Menu>
 </Menubar.Root>
