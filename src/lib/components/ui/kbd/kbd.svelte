bb780483b6d5b0c812f3d23bec24548b4890a770
Can Baykar <togancan42@gmail.com>
2026-01-20 12:28:05 +0300

    refactor(ui): Disabled style

--- src/lib/components/ui/kbd/kbd.svelte
+++ src/lib/components/ui/kbd/kbd.svelte
@@ -16,7 +16,8 @@
 	class={cn(
 		"bg-bg-overlay text-fg-muted border-border-muted/75 border border-b-2 pointer-events-none inline-flex h-5 w-fit min-w-5 items-center justify-center gap-1 rounded-sm px-1 font-sans text-xs font-medium select-none",
 		"[&_svg:not([class*='size-'])]:size-3",
-		"[[data-slot=tooltip-content]_&]:bg-bg/20 [[data-slot=tooltip-content]_&]:text-bg",
+		"[[data-slot=tooltip-content]_&]:bg-bg/20 [[data-slot=tooltip-content]_&]:text-bg [[data-slot=tooltip-content]_&]:border-bg/75",
+		"[[data-disabled]_&]:opacity-50",
 		className
 	)}
 	{...restProps}
