6d35e03f50f05a7799dc614dbf14356f250cb284
Can Baykar <togancan42@gmail.com>
2026-02-24 07:06:44 +0300

    fix: meta tag image with invalid url
    This also prevents the app from deploying on GitHub Pages for some reason.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,29 +3,30 @@ import { defineBaseMetaTags } from 'svelte-meta-tags';
 export const prerender = true;
 
 export const load = ({ url }) => {
-  const baseTags = defineBaseMetaTags({
-    title: '404',
-    titleTemplate: '%s | Twig',
-    description: 'Twig is an educational proof assistant. Make Gentzen-style derivations via drag-and-drop.',
-    canonical: new URL(url.pathname, url.origin).href, // creates a cleaned up URL (without hashes or query params) from your current URL
-    openGraph: {
-      type: 'website',
-      url: new URL(url.pathname, url.origin).href,
-      locale: 'en_IE',
-      title: 'Twig | Educational Proof Assistant',
-      description: 'Twig is an educational proof assistant. Make Gentzen-style derivations via drag-and-drop.',
-      siteName: 'Twig',
-      images: [
-        {
-          url: '/branding/banner.png',
-          alt: 'Twig: Educational Proof Assistant',
-          width: 1280,
-          height: 640,
-          type: 'image/png'
-        }
-      ]
-    }
-  });
 
-  return { ...baseTags };
+	const baseTags = defineBaseMetaTags({
+		title: '404',
+		titleTemplate: '%s | Twig',
+		description: 'Twig is an educational proof assistant. Make Gentzen-style derivations via drag-and-drop.',
+		canonical: new URL(url.pathname, url.origin).href, // creates a cleaned up URL (without hashes or query params) from your current URL
+		openGraph: {
+			type: 'website',
+			url: new URL(url.pathname, url.origin).href,
+			locale: 'en_IE',
+			title: 'Twig | Educational Proof Assistant',
+			description: 'Twig is an educational proof assistant. Make Gentzen-style derivations via drag-and-drop.',
+			siteName: 'Twig',
+			images: [
+				{
+					url: new URL('/branding/banner.png', url.origin).href,
+					alt: 'Twig: Educational Proof Assistant',
+					width: 1280,
+					height: 640,
+					type: 'image/png'
+				}
+			]
+		}
+	});
+
+	return { ...baseTags };
 };
